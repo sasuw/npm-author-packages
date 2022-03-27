0ed9322c6a6839a4287da4c758406f8dab85f421
Sasu <git@sasu.net>
2022-03-27 18:27:08 +0200

    Improved formatting, refactoring

--- bin/index.js
+++ bin/index.js
@@ -1,32 +1,109 @@
 #!/usr/bin/env node
 
-const readNpmPackageAuthors = require("../lib/readNpmPackageAuthors.js");
+const readNpmPackageAuthors = require('../lib/readNpmPackageAuthors.js');
+const ObjectUtils = require('../lib/objectUtils');
+const { spawn } = require('child_process');
+const commandLineArgs = require('command-line-args')
+const commandLineUsage = require('command-line-usage')
+const chalk = require('chalk');
 
-(async function () {
-    if (process.argv.length < 2) {
-        console.log('You have to provide a npm author.');
-        process.exit(3);
-    }
+function printToConsole(msg){
+    process.stdout.write(msg + '\n');
+}
 
-    if (process.argv.length > 3) {
-        console.log('Too many arguments.');
-        process.exit(4);
-    }
+async function fetchAuthorPackages(npmUser) {
+    return readNpmPackageAuthors.readNpmPackageAuthors(npmUser);
+}
 
-    let npmUser = process.argv[2];
-    let authorPackages = await readNpmPackageAuthors.readNpmPackageAuthors(npmUser);
-
-    if (authorPackages.length === 0) {
-        console.log('No packages found.');
+function printAuthorPackages(authorPackages) {
+    if (authorPackages.length === 0) {  
+        printToConsole('No packages found.');
     } else {
         let packageNoun = 'packages';
         if (authorPackages.length === 1) {
             packageNoun = 'package';
         }
-        console.log(authorPackages.length + ' ' + packageNoun + ' found:');
-    }
+        let foundStr = authorPackages.length + ' ' + packageNoun + ' found';
+        printToConsole(chalk.underline(foundStr));
+    }  
 
     authorPackages.forEach(element => {
-        console.log(element);
+        printToConsole(element);
     });
+}
+
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+(async function () {
+    const optionDefinitions = [
+        { name: 'quiet', alias: 'q', type: Boolean },
+        { name: 'author', alias: 'a', type: String, multiple: false, defaultOption: true },
+        { name: 'package', alias: 'p', type: String },
+        { name: 'help', alias: 'h', type: String },
+        { name: 'version', alias: 'v', type: String }
+    ]
+
+    const options = commandLineArgs(optionDefinitions);
+
+    console.log(options);
+
+    if (ObjectUtils.isEmpty(options) || options.help !== undefined) {
+        const sections = [
+            {
+                header: 'npm-author-packages',
+                content: 'outputs npm author package relations'
+            },
+            {
+                header: 'Options',
+                optionList: [
+                    {
+                        name: 'quiet',
+                        alias: 'q',
+                        description: 'does not output any extraneous information'
+                    },
+                    {
+                        name: 'npm-author',
+                        alias: 'a',
+                        typeLabel: '{underline npm-author-username}',
+                        description: 'Username of npm-author, whose packages you want to list'
+                    },
+                    {
+                        name: 'package',
+                        alias: 'p',
+                        typeLabel: '{underline npm-package-name}',
+                        description: 'Name of npm package, for which you want to find out whether the given npm-author has been involved in. Requires --author option to be supplied.'
+                    },
+                    {
+                        name: 'help',
+                        alias: 'h',
+                        description: 'Print this usage guide.'
+                    },
+                    {
+                        name: 'version',
+                        alias: 'v',
+                        description: 'Prints version number of npm-author-package'
+                    }
+                ]
+            }
+        ]
+        const usage = commandLineUsage(sections);
+        printToConsole(usage);
+        process.exit(0);
+    }
+
+    //process.exit(0);
+    if (options.author == null) {
+        printToConsole('You have to provide a npm author.');
+        process.exit(3);
+    }
+
+    let t1 = process.hrtime.bigint();
+    let authorPackages = await fetchAuthorPackages(options.author);
+    let t2 = process.hrtime.bigint();
+    let elapsedTimeNs = t2 - t1;
+    let elapsedTime = (elapsedTimeNs.toString().slice(0, -6) + ' ms');
+    printToConsole('Executing npm-author-packages took ' + elapsedTime + '\n');
+    printAuthorPackages(authorPackages);
 })();
