f668dcc617f17842f38a88d7b695c7702c5b6751
Sasu <git@sasu.net>
2022-03-28 10:48:05 +0200

    Refactoring, added implementation for --version flag

--- bin/index.js
+++ bin/index.js
@@ -6,8 +6,9 @@ const { spawn } = require('child_process');
 const commandLineArgs = require('command-line-args')
 const commandLineUsage = require('command-line-usage')
 const chalk = require('chalk');
+const getPackageVersion = require('../lib/version');
 
-function printToConsole(msg){
+function printToConsole(msg) {
     process.stdout.write(msg + '\n');
 }
 
@@ -15,27 +16,27 @@ async function fetchAuthorPackages(npmUser) {
     return readNpmPackageAuthors.readNpmPackageAuthors(npmUser);
 }
 
+function printAuthorPackageInvolvement(npmUser, npmPackage){
+    printToConsole('Not implemented yet.');
+}
+
 function printAuthorPackages(authorPackages, quietOptionEnabled) {
-    if (authorPackages.length === 0) {  
+    if (authorPackages.length === 0) {
         printToConsole('No packages found.');
     } else {
         let packageNoun = 'packages';
         if (authorPackages.length === 1) {
             packageNoun = 'package';
         }
-        if(!quietOptionEnabled){
+        if (!quietOptionEnabled) {
             let foundStr = authorPackages.length + ' ' + packageNoun + ' found';
             printToConsole(chalk.underline(foundStr));
         }
-    }  
+    }
 
     authorPackages.forEach(element => {
         printToConsole(element);
     });
-}
-
-function isEmpty(obj) {
-    return Object.keys(obj).length === 0;
 }
 
 (async function () {
@@ -43,56 +44,27 @@ function isEmpty(obj) {
         { name: 'quiet', alias: 'q', type: Boolean },
         { name: 'author', alias: 'a', type: String, multiple: false, defaultOption: true },
         { name: 'package', alias: 'p', type: String },
-        { name: 'help', alias: 'h', type: String },
-        { name: 'version', alias: 'v', type: String }
+        { name: 'help', alias: 'h', type: Boolean },
+        { name: 'version', alias: 'v', type: Boolean }
     ]
 
     const options = commandLineArgs(optionDefinitions);
 
-    //console.log(options);
+    console.log(options);
 
-    if (ObjectUtils.isEmpty(options) || options.help !== undefined) {
-        const sections = [
-            {
-                header: 'npm-author-packages',
-                content: 'outputs npm author package relations'
-            },
-            {
-                header: 'Options',
-                optionList: [
-                    {
-                        name: 'quiet',
-                        alias: 'q',
-                        description: 'does not output any extraneous information'
-                    },
-                    {
-                        name: 'npm-author',
-                        alias: 'a',
-                        typeLabel: '{underline npm-author-username}',
-                        description: 'Username of npm-author, whose packages you want to list'
-                    },
-                    {
-                        name: 'package',
-                        alias: 'p',
-                        typeLabel: '{underline npm-package-name}',
-                        description: 'Name of npm package, for which you want to find out whether the given npm-author has been involved in. Requires --author option to be supplied.'
-                    },
-                    {
-                        name: 'help',
-                        alias: 'h',
-                        description: 'Print this usage guide.'
-                    },
-                    {
-                        name: 'version',
-                        alias: 'v',
-                        description: 'Prints version number of npm-author-package'
-                    }
-                ]
-            }
-        ]
-        const usage = commandLineUsage(sections);
-        printToConsole(usage);
+    if (ObjectUtils.isEmpty(options) || options.help) {
+        displayHelp();
         process.exit(0);
+    }
+
+    if (options.version) {
+        printToConsole('npm-package-authors v. ' + getPackageVersion());
+        process.exit(0);
+    }
+
+    if (options.package != null && options.author == null) {
+        printToConsole('Option --package (-p) requires that you also supply the npm author with the --author (-a) option. For more information, run this command with --help.');
+        process.exit(4);
     }
 
     //process.exit(0);
@@ -101,16 +73,62 @@ function isEmpty(obj) {
         process.exit(3);
     }
 
-    let quietOptionEnabled = (options.quiet !== undefined);
-    if(!quietOptionEnabled){
-        var t1 = process.hrtime.bigint();
+    if (options.package != null && options.author != null) {
+        printAuthorPackageInvolvement(options.author, options.package);
+    }else{
+        if (!options.quiet) {
+            var t1 = process.hrtime.bigint();
+        }
+        let authorPackages = await fetchAuthorPackages(options.author);
+        if (!options.quiet) {
+            let t2 = process.hrtime.bigint();
+            let elapsedTimeNs = t2 - t1;
+            let elapsedTime = (elapsedTimeNs.toString().slice(0, -6) + ' ms');
+            printToConsole('Executing npm-author-packages took ' + elapsedTime + '\n');
+        }
+        printAuthorPackages(authorPackages, options.quiet);
     }
-    let authorPackages = await fetchAuthorPackages(options.author);
-    if(!quietOptionEnabled){
-        let t2 = process.hrtime.bigint();
-        let elapsedTimeNs = t2 - t1;
-        let elapsedTime = (elapsedTimeNs.toString().slice(0, -6) + ' ms');
-        printToConsole('Executing npm-author-packages took ' + elapsedTime + '\n');
-    }
-    printAuthorPackages(authorPackages, quietOptionEnabled);
 })();
+
+function displayHelp() {
+    const sections = [
+        {
+            header: 'npm-author-packages',
+            content: 'outputs npm author package relations'
+        },
+        {
+            header: 'Options',
+            optionList: [
+                {
+                    name: 'quiet',
+                    alias: 'q',
+                    description: 'does not output any extraneous information'
+                },
+                {
+                    name: 'npm-author',
+                    alias: 'a',
+                    typeLabel: '{underline npm-author-username}',
+                    description: 'Username of npm-author, whose packages you want to list'
+                },
+                {
+                    name: 'package',
+                    alias: 'p',
+                    typeLabel: '{underline npm-package-name}',
+                    description: 'TBD: Name of npm package, for which you want to find out whether the given npm-author has been involved in. Requires --author option to be supplied.'
+                },
+                {
+                    name: 'help',
+                    alias: 'h',
+                    description: 'Print this usage guide.'
+                },
+                {
+                    name: 'version',
+                    alias: 'v',
+                    description: 'Prints version number of npm-author-package'
+                }
+            ]
+        }
+    ]
+    const usage = commandLineUsage(sections);
+    printToConsole(usage);
+}
